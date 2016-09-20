a23517f6280c4b9f481fb0288cc7c866134c149d
Артем Прокопович < >
2016-09-20 22:52:01 +0300

    point 2a-в, 3 implemented

--- COS Project/js/custom/first.js
+++ COS Project/js/custom/first.js
@@ -1,91 +1,131 @@
-window.onload = function () {
-	var N = 512;
-	var fields_first_2a_fi = ["#first-2a-fi1","#first-2a-fi2","#first-2a-fi3","#first-2a-fi4","#first-2a-fi5"];
-	var field_first_2a_A = "#first-2a-A";
-	var field_first_2a_f = "#first-2a-f";
-	var first_2a_button = "#first-2a-button";
+window.onload = function() {
+    var N = 512;
+    var $fields_first_2a_fi = $('[id ^= first-2a-fi]');
+    var $field_first_2a_A = $('#first-2a-A');
+    var $field_first_2a_f = $("#first-2a-f");
+    var $first_2a_button = $("#first-2a-button");
 
-	var field_first_2b_fi = "#first-2b-fi";
-	var field_first_2b_A = "#first-2b-A";
-	var fields_first_2b_f = ["#first-2b-f1","#first-2b-f2","#first-2b-f3","#first-2b-f4","#first-2b-f5"];
-	var first_2b_button = "#first-2b-button";
+    var $field_first_2b_fi = $("#first-2b-fi");
+    var $field_first_2b_A = $("#first-2b-A");
+    var $fields_first_2b_f = $("[id ^=first-2b-f]:not('#first-2b-fi')");
+    var $first_2b_button = $("#first-2b-button");
 
-	var field_first_2c_fi = "#first-2c-fi";
-	var fields_first_2c_A = ["#first-2c-A1","#first-2c-A2","#first-2c-A3","#first-2c-A4","#first-2c-A5"];
-	var field_first_2c_f = "#first-2c-f";
-	var first_2c_button = "#first-2c-button";
+    var $field_first_2c_fi = $('#first-2c-fi');
+    var $fields_first_2c_A = $('[id ^= first-2c-A]');
+    var $field_first_2c_f = $('#first-2c-f');
+    var $first_2c_button = $('#first-2c-button');
 
-	var fields_first_2d_fi = ["#first-2d-fi1","#first-2d-fi2","#first-2d-fi3","#first-2d-fi4","#first-2d-fi5"];
-	var field_first_2d_A = "#first-2d-A";
-	var field_first_2d_f = "#first-2d-f";
-	var first_2d_button = "#first-2d-button";
+    var $fields_first_3_fi = $('[id ^= first-3-fi]');
+    var $field_first_3_A = $("#first-3-A");
+    var $field_first_3_f = $("#first-3-f");
+    var $first_3_button = $("#first-3-button");
 
-	var chart = new CanvasJS.Chart("first-work", {
-		zoomEnabled: true,
-		animationEnabled: true,
-		data: [ { 
-			type: "spline", 
-			dataPoints: [ { x: 0, y: 0 } ] ,
-		}],
-		legend: 
-		{
-			cursor: "pointer",
-			itemclick: function (e) {
-				if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-					e.dataSeries.visible = false;
-				} else {
-					e.dataSeries.visible = true;
-			}
-			chart.render();
-			}
-		}
-	});
+    var chart = new CanvasJS.Chart("first-work", {
+        zoomEnabled: true,
+        animationEnabled: true,
+        data: [{
+            type: "spline",
+            dataPoints: [{ x: 0, y: 0 }],
+        }],
+        legend: {
+            cursor: "pointer",
+            itemclick: function(e) {
+                if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                } else {
+                    e.dataSeries.visible = true;
+                }
+                chart.render();
+            }
+        }
+    });
 
-	chart.render();
+    chart.render();
 
-	function drawGraph (data, number)
-	{
-		chart.options.data[number] = data;
-		chart.render();
+    function drawGraph(data, number) {
+        chart.options.data[number] = data;
+        chart.render();
     }
 
-	function calculateFunction2(a, fi, f)
-	{
-		var result = [];
-		for(var n = 0; n < N - 1; n++)
-		{
-			result[n] = {};
-			result[n].x = n;
-			result[n].y = a * Math.sin(((2 * Math.PI * f * n) / N) + fi * Math.PI);
-		}
-		return result;
-	}
+    function clearGraph() {
+        chart.options.data = [{
+            type: "spline",
+            dataPoints: [{ x: 0, y: 0 }],
+        }];
+        chart.render();
+    }
 
-	$(first_2a_button).click(function()
-	{
-		for(var i = 0; i < fields_first_2a_fi.length; i++)
-		{
-			var result = {};
-			var text = $(fields_first_2a_fi[i]).val();
-			result.dataPoints = calculateFunction2($(field_first_2a_A).val(), text , $(field_first_2a_f).val());
-			result.type = "spline";
-			result.showInLegend = true;
-			result.name = text;  
-			drawGraph(result, i + 1);
-		}
-	});
+    function calculateFunction2(a, fi, f) {
+        var result = [];
+        for (var n = 0; n < N; n++) {
+            result[n] = {};
+            result[n].x = n;
+            result[n].y = a * Math.sin(((2 * Math.PI * f * n) / N) + fi * Math.PI);
+        }
+        return result;
+    }
 
-	$(first_2b_button).click(function()
-	{
-		for(var i = 0; i < fields_first_2b_f.length; i++)
-		{
-			var result = {};
-			var text = $(fields_first_2b_f[i]).val();
-			result.dataPoints = calculateFunction2($(field_first_2b_A).val(), $(field_first_2b_fi).val(), text);
-			result.type = "spline";
-			result.showInLegend = true;
-			result.name = text; 
-			drawGraph(result, i + 1);
-		}
-	});
+    $first_2a_button.click(function() {
+        clearGraph();
+        for (var i = 0; i < $fields_first_2a_fi.length; i++) {
+            var result = {};
+            var text = $($fields_first_2a_fi[i]).val();
+            result.dataPoints = calculateFunction2($field_first_2a_A.val(), text, $field_first_2a_f.val());
+            result.type = "spline";
+            result.showInLegend = true;
+            result.name = text;
+            drawGraph(result, i + 1);
+        }
+    });
+
+    $first_2b_button.click(function() {
+        clearGraph();
+        for (var i = 0; i < $fields_first_2b_f.length; i++) {
+            var result = {};
+            var text = $($fields_first_2b_f[i]).val();
+            result.dataPoints = calculateFunction2($field_first_2b_A.val(), $field_first_2b_fi.val(), text);
+            result.type = "spline";
+            result.showInLegend = true;
+            result.name = text;
+            drawGraph(result, i + 1);
+        }
+    });
+
+    $first_2c_button.click(function() {
+        clearGraph();
+        for (var i = 0; i < $fields_first_2c_A.length; i++) {
+            var result = {};
+            var text = $($fields_first_2c_A[i]).val();
+            result.dataPoints = calculateFunction2(text, $field_first_2c_fi.val(), $field_first_2c_f.val());
+            result.type = "spline";
+            result.showInLegend = true;
+            result.name = text;
+            drawGraph(result, i + 1);
+        }
+    });
+
+    $first_3_button.click(function() {
+        clearGraph();
+        var dataPointsArray = [];
+        for (var i = 0; i < $fields_first_3_fi.length; i++) {
+            dataPointsArray[i] = calculateFunction2($field_first_3_A.val(), $($fields_first_3_fi[i]).val(), $field_first_2c_f.val());
+        }
+        var dataPoints = [];
+        for (var i = 0; i < N; i++) {
+            var sum = 0;
+            for (var j = 0; j < dataPointsArray.length; j++) {
+                sum += dataPointsArray[j][i].y;
+            }
+            dataPoints[i] = {
+                x: i,
+                y: sum
+            };
+        }
+        var result = {};
+        result.dataPoints = dataPoints;
+        result.type = "spline";
+        result.showInLegend = true;
+        result.name = "Polygarmonic_function";
+        drawGraph(result, 1);
+    });
 }
